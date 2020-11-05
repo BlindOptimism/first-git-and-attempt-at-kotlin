de759a966e8cf6a511f2d556526174d37c983f8c
BlindOptimism <warren.kuyk@gmail.com>
2020-11-05 19:40:53 +0200

    initial commit

---
+++ IncomePlanner/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IncomePlanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SubmitBtn_Clicked(object sender, EventArgs e)
+        {
+            _ = label.RelRotateTo(360, 1000);
+            var expenseVal = double.Parse(expenses.Text);
+            var perHourVal = double.Parse(perHour.Text);
+            var hoursVal = double.Parse(hours.Text);
+            var overtimeVal = double.Parse(overtime.Text);
+
+            var total = (perHourVal * hoursVal) + overtimeVal - expenseVal;
+
+            label.Text = $"$ {total}";
+        }
+    }
+}
